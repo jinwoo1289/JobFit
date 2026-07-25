c681694fd7ae1ca90c1bba2a653321cd19bab62a
DESKTOP-GO177QK\jinwo <jinwoo2202@naver.com>
2026-07-25 15:03:47 +0900

    Init Project

---
+++ src/main/java/com/jinwoo/jobfit/JobfitApplication.java
@@ -0,0 +1,13 @@
+package com.jinwoo.jobfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobfitApplication.class, args);
+	}
+
+}
